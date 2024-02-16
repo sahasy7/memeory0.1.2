2b8d3c15726e4961abe068fdac826d4843d13164
sahasy7 <74914769+sahasy7@users.noreply.github.com>
2024-02-16 12:56:54 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,111 @@
+import os
+import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains.conversation.memory import ConversationSummaryMemory
+from langchain.chains import ConversationalRetrievalChain,LLMChain, RetrievalQA
+
+# Set page configuration for Streamlit
+st.set_page_config(page_title="Chat with the Chat Bot",
+                   page_icon="🤖",
+                   layout="centered",
+                   initial_sidebar_state="auto",
+                   menu_items=None)
+
+# Title for the app
+st.title("Welcome To GSM infoBot")
+
+# Initialize the chat messages history
+if "messages" not in st.session_state.keys():
+  st.session_state.messages = [{
+      "role":
+      "assistant",
+      "content":
+      "Need Info? Ask Me Questions about GSM Mall's Features"
+  }]
+
+os.environ['OPENAI_API_KEY'] = st.secrets.openai_key
+
+@st.cache(show_spinner=False)
+def load_data():
+  with st.spinner(
+      text=
+      "Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."
+  ):
+        loader = TextLoader("data/GSM Mall Update Q&A.txt")
+        documents = loader.load()
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_documents(documents)
+        embeddings = OpenAIEmbeddings()
+        db = FAISS.from_documents(texts, embeddings)
+        return db
+
+vectore_store = load_data()
+dr = vectore_store.as_retriever()
+
+
+prompt_template = """
+Your friendly assistant is here to help! Remember, always provide clear, concise, and friendly responses within 10-15 words. value User time and aim to provide clear and concise responses. Maintain a positive and professional tone. Encourage users to visit the store subtly, without being pushy. Dont hallucinate. Let's make every interaction a delightful experience! 😊
+
+You will be given a context of the conversation made so far followed by a customer's question,
+give the answer to the question using the context.
+The answer should be short, straight and to the point. If you don't know the answer, reply that the answer is not available.
+
+Context: {context}
+
+Question: {question}
+Answer:"""
+
+PROMPT = PromptTemplate(
+template=prompt_template, input_variables=["context", "question"]
+)
+
+chain_type_kwargs = { "prompt" : PROMPT }
+#build your LLM
+llm = ChatOpenAI(temperature=0)
+
+template = (
+                """Combine the chat history and follow up question into
+                a standalone question.
+                If chat hsitory is empty, use the follow up question as it is.
+                Chat History: {chat_history}
+                Follow up question: {question}"""
+            )
+        # TRY TO ADD THE INPUT VARIABLES
+prompt = PromptTemplate.from_template(template)
+        # question_generator_chain = LLMChain(llm=llm, prompt=prompt
+if "chat_engine" not in st.session_state.keys():
+  # Initialize the chat engine
+  st.session_state.chat_engine = ConversationalRetrievalChain.from_llm(
+            llm = llm,
+            chain_type = "stuff",
+            memory = ConversationSummaryMemory(llm = llm, memory_key='chat_history', input_key='question', output_key= 'answer', return_messages=True),
+            retriever = dr,
+            condense_question_prompt = prompt,
+            return_source_documents=False,
+            combine_docs_chain_kwargs=chain_type_kwargs,
+        )
+
+if prompt := st.text_input("Your question"):
+  # Prompt for user input and save to chat history
+  st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages:
+  # Display the prior chat messages
+  with st.chat_message(message["role"]):
+    st.write(message["content"])
+
+# If the last message is not from the assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+  with st.chat_message("assistant"):
+    with st.spinner("Thinking..."):
+      response = st.session_state.chain(
+                {"context": st.session_state.chat_engine.memory.buffer, "question": customer_prompt}, return_only_outputs=True)
+      st.write(response.response)
+      message = {"role": "assistant", "content": response.response}
+      st.session_state.messages.append(
+          message)  # Add response to message history
